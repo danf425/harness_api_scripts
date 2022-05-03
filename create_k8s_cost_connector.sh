496cc8e8e29a4c52e38bd506be0ab18b23e7d8f7
Dan Flores Montanez <danf425@gmail.com>
2022-05-03 12:39:06 -0400

    Update create_k8s_cost_connector.sh

--- create_k8s_cost_connector.sh
+++ create_k8s_cost_connector.sh
@@ -11,6 +11,7 @@
 account_id="your_account_id"
 #Token-creation docs: https://ngdocs.harness.io/article/tdoad7xrh9-add-and-manage-api-keys#create_personal_access_token 
 token="your_harness_token"
+SAtoken_id="account.YOURSECRETIDENTIFIER"
 description="API provisioned k8s connectors"
 
 #define list of masterUrls and names below:
@@ -45,7 +46,7 @@ curl -i -X POST \
                 "auth": {
                   "type": "ServiceAccount",
                   "spec": {
-                    "serviceAccountTokenRef": "account.YOURSECRETIDENTIFIER",
+                    "serviceAccountTokenRef": "'"$SAtoken_id"'",
                     "caCertRef": null
                   }
                 }
